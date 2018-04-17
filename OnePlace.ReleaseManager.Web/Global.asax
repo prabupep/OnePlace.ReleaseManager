e5e73a5b205b2e6df5d76e46216749a253e11595
praburaj.pandian <praburaj.pandian@CDC2-L-54650LS>
2018-04-17 11:35:18 +0530

    Add project files.

---
+++ OnePlace.ReleaseManager.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnePlace.ReleaseManager.Web.MvcApplication" Language="C#" %>
---
+++ OnePlace.ReleaseManager.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnePlace.ReleaseManager.Web.Startup))]
+namespace OnePlace.ReleaseManager.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
